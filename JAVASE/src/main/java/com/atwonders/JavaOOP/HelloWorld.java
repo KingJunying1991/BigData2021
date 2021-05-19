19e8fa84fbc585d5a1d46790381cf46e79b0a7ff
kingJunying <wnostop@gmail.com>
2021-05-19 11:45:41 +0800

    Initial commit

---
+++ JAVASE/src/main/java/com/atwonders/JavaOOP/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atwonders.JavaOOP;
+
+/**
+ * HelloWorld
+ *
+ * @author King
+ * @date 2021/5/19 11:04
+ * @since 1.0.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
